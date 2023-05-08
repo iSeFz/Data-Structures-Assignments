1d162fc80af1a833f66b5b3c36e29e57130c35d2
iSeFz <seifyahia@gmail.com>
2023-05-09 00:46:51 +0300

    Modified the main menu & included the BST data structure TODO: Reading from file is incorrect and needs fixing

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
-#include "Student.hpp"
 #include "Structures/AVL.cpp"
+#include "Structures/BST.cpp"
 
+// Append student data from file into vector
 void readFile(ifstream& file, vector<Student>& students){
     int studentCount, count = 0;
     string line, data, name, dept, id, gpa;
@@ -24,9 +25,9 @@ void readFile(ifstream& file, vector<Student>& students){
             data = "";
         }
     }
-
 }
 
+// Driver function for the AVL data structure
 void useAVL(vector<Student>& students){
     AVL<Student>avl;
     for (int i = 0; i < students.size(); ++i) {
@@ -35,43 +36,44 @@ void useAVL(vector<Student>& students){
     avl.menu();
 }
 
-
-
-
-void mainMenu(vector<Student>& students){
-    int choice;
-    cout << "Choose Data Structure:\n\t"
-    << "1. BST\n\t2. AVL\n\t3. Min Heap\n\t4. Max Heap\n\t5. Exit\n";
-    cout << "\nEnter number of option: ";
-    cin >> choice;
-    while (choice != 5){
-        switch (choice) {
-            case 1: // BST menu
-                break;
-            case 2:
-                useAVL(students);
-                break;
-            case 3: // Min heap
-                break;
-            case 4: // Max heap
-                break;
-            default: break;
-        }
-        cout << "Choose Data Structure:\n\t"
-             << "1. BST\n\t2. AVL\n\t3. Min Heap\n\t4. Max Heap\n\t5. Exit\n";
-        cout << "\nEnter number of option: ";
-        cin >> choice;
-    }
+// Driver function for the BST data structure
+void useBST(vector<Student>& students){
+    BST bst;
+    for(auto stud : students)
+        bst.addNewStudent(stud);
+    bst.menu();
 }
 
-
-
-
+// Main function to test the whole program
 int main() {
     vector<Student> students;
     ifstream file;
     file.open("students.txt");
     readFile(file, students);
-    mainMenu(students);
+    // Output the main menu
+    short choice;
+    while(true){
+        cout << "\tAvailable Data Structures\n"
+        << "1. BST\n"
+        << "2. AVL\n"
+        << "3. Min Heap\n"
+        << "4. Max Heap\n"
+        << "5. Exit\n"
+        << "Choose one of the above options (1-5) >> ";
+        choice = 0;
+        cin >> choice;
+        if(choice == 1)
+            useBST(students);
+        else if(choice == 2)
+            useAVL(students);
+        else if(choice == 3)
+            cout << "Coming Soon!!\n";
+        else if(choice == 4)
+            cout << "Coming Soon!!\n";
+        else if(choice == 5) break;
+        else
+            cerr << "\n\tINVALID INPUT!! Enter ONLY numbers from 1 to 5\n";
+    }
+    cout << "\tThank you for using our program!\n";
     return 0;
 }
