f4a7a5b4d0239481b05d2cfc71f2aa722e0d4ae1
iSeFz <seifyahia@gmail.com>
2023-05-09 00:39:19 +0300

    Modified the Student class to be working temporarily Added Setter methods + some comments

--- Student.hpp
+++ Student.hpp
@@ -1,78 +1,78 @@
 #include <iostream>
 using namespace std;
-class Student
-{
+
+// Student Class
+class Student{
 private:
     string name;
-    int id;
-    float gpa;
+    string id;
+    string gpa;
     string dept;
-
 public:
-    Student() : name(""), id(0), gpa(0), dept("") {}
-    Student(string id) : id(stoi(id)) {}
-    Student(string name, string gpa, string dept, string id)
-    {
+    // Constructors
+    Student() : name(""), id("0"), gpa("0"), dept("") {}
+    Student(string id) : id(id) {}
+    Student(string name, string gpa, string dept, string id){
         this->name = name;
-        this->gpa = stof(gpa);
+        this->gpa = gpa;
         this->dept = dept;
-        this->id = stoi(id);
+        this->id = id;
     }
 
-    int getId()
-    {
-        return id;
-    }
+    // Getter methods to get student data
+    string getId(){ return id; }
+    string getName(){ return name; }
+    string getGpa(){ return gpa; }
+    string getDept(){ return dept; }
 
-    string getName()
-    {
-        return name;
-    }
+    // Setter methods to set student data
+    void setId(string id){ this->id = id; }
+    void setName(string name){ this->name = name; }
+    void setGpa(string gpa){ this->gpa = gpa; }
+    void setDept(string dept){ this->dept = dept; }
 
-    float getGpa()
-    {
-        return gpa;
-    }
-
-    string getDept()
-    {
-        return dept;
-    }
-
+    // Overloading the comparison operators to compare students
     bool operator < (Student& another) const{
         return (this->id < another.id);
     }
-
     bool operator > (Student& another) const{
         return (this->id > another.id);
     }
-
     bool operator == (Student& another) const{
         return (this->id == another.id);
     }
-
     bool operator != (Student& another) const{
         return (this->id != another.id);
     }
-
+    // Friend functions to overload the extraction and insertion operators
     friend ostream& operator << (ostream& out,Student& s);
     friend istream& operator >> (istream& in, Student& s);
 };
 
+// Overloading the extraction operator
 ostream& operator << (ostream& out,Student& s){
     out << "[" << s.id << ", " << s.name << ", " << s.gpa << ", " << s.dept << "]\n";
     return out;
 }
+
+// Overloading the insertion operator
 istream& operator >> (istream& in, Student& s){
-    cout << "Id: ";
-    in >> s.id;
-    cout << "Name: ";
+    string id, name, gpa, dept;
+    cout << "Enter student ID >> ";
+    in >> id;
+    s.setId(id);
+    cout << "Enter student Name >> ";
     in.clear();
     in.ignore();
-    getline(in,s.name);
-    cout << "GPA: ";
-    in >> s.gpa;
-    cout << "Department: ";
-    in >> s.dept;
+    getline(in,name);
+    s.setName(name);
+    cout << "Enter student GPA >> ";
+    in.clear();
+    in >> gpa;
+    s.setGpa(gpa);
+    cout << "Enter student Department >> ";
+    in.clear();
+    in >> dept;
+    s.setDept(dept);
     return in;
 }
