f37925614e644084c78d05f0f86d5132c76f63d4
iSeFz <seifyahia@gmail.com>
2023-05-13 01:26:19 +0300

    Moved main menu of BST to main.cpp

--- main.cpp
+++ main.cpp
@@ -30,11 +30,49 @@ void readFile(ifstream& file, vector<Student>& students){
 }
 
 // Driver function for the BST data structure
-void useBST(const vector<Student>& students){
-    BST bst;
-    for(auto stud : students)
-        bst.addNewStudent(stud);
-    bst.menu();
+void useBST(const vector<Student> &students){
+    // Initialize the tree with current students
+    BST binaryTree(students);
+    // Binary Search Tree Menu
+    short choice;
+    while(true){
+        cout << "\n\tBinary Search Tree Menu\n"
+        << "1. Add new student\n"
+        << "2. Remove student by ID\n"
+        << "3. Search student by ID\n"
+        << "4. Print All Students (Sorted by ID)\n"
+        << "5. Return to main menu\n"
+        << "Choose one of the above options (1-5) >> ";
+        cin >> choice;
+        // Get student data from the user
+        if(choice == 1){
+            cout << "\n";
+            Student newStudent;
+            cin >> newStudent;
+            binaryTree.addNewStudent(newStudent);
+            cout << "Student Added Successfully!\n";
+        }
+        // Remove certain student using ID
+        else if(choice == 2){
+            short id;
+            cout << "\nEnter ID of student to remove >> ";
+            cin >> id;
+            binaryTree.removeStudent(id);
+        }
+        // Search for certain student using ID
+        else if(choice == 3){
+            short id;
+            cout << "\nEnter ID of student to search for >> ";
+            cin >> id;
+            binaryTree.searchByID(id);
+        }
+        // Output all current students sorted by ID
+        else if(choice == 4)
+            binaryTree.printAll();
+        else if(choice == 5) break;
+        else
+            cerr << "\n\tINVALID INPUT!! Enter ONLY numbers from 1 to 5\n";
+    }
 }
 
 // Driver function for the AVL data structure
@@ -92,6 +130,7 @@ void minHeap(const vector<Student>& students){
         else cerr << "\n\tINVALID INPUT!! Enter ONLY numbers from 1 to 3\n";
     }
 }
+
 // Main function to test the whole program
 int main() {
     vector<Student> students;
@@ -100,7 +139,7 @@ int main() {
     // Output the main menu
     short choice;
     while(true){
-        cout << "\tAvailable Data Structures\n"
+        cout << "\n\tAvailable Data Structures\n"
         << "1. BST\n"
         << "2. AVL\n"
         << "3. Min Heap\n"
